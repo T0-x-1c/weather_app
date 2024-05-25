9a5f3e96f7681420b376108339124efa353bdf3e
sem_naz <semerenkonazar90@gmail.com>
2024-05-25 12:32:04 +0300

    урок 3

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
+import requests
+from config import API_KEY, API_URL
+
 
 class WeatherCard(MDCard):
     def __init__(self, *args, **kwargs):
@@ -11,6 +14,18 @@ class WeatherCard(MDCard):
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def weather_search(self):
+        city = self.ids.city_field.text.strip().lower() #отримуємо з стайл кв назву міста
+        api_params = {
+            'q': city,
+            'appid': API_KEY
+        }
+        data = requests.get(API_URL, api_params)# запит до сервісу погоди з параметрами
+        response = data.json()#отримуємо в-дь у форматі json
+        print(response)
+        description = response["weather"][0]["description"]
+        self.ids.weather_card.ids.label.text = description
 
 class MainApp(MDApp):
     def build(self):
