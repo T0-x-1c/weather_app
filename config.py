52c123c47414fa6a166f12d1f7ddbbf83a65903f
sem_naz <semerenkonazar90@gmail.com>
2024-06-08 12:48:28 +0300

    урок 5

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 API_KEY = "4170c1d06aac3e2c19cfe31ff11640c4"
 API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=uk"
+FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?&cnt=24&units=metric&lang=uk"
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 import requests
-from config import API_KEY, API_URL
+from config import API_KEY, API_URL, FORECAST_URL
 import json
 
 with open('setting.json', 'r', encoding='utf-8') as file:
@@ -16,7 +16,7 @@ def save():
 
 
 class WeatherCard(MDCard):
-    def __init__(self, description, icon, humidity, temp, rain, wind, feel_like, city_name, *args, **kwargs):
+    def __init__(self, description, icon, humidity, temp, rain, wind, feel_like, city_name, date_time, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ids.desc_text.text = description
         self.ids.temp_text.text = f"{temp}°C"
@@ -26,42 +26,67 @@ class WeatherCard(MDCard):
         self.ids.wind_text.text = f"Вітер: {wind} км/год"
         self.ids.weather_icon.source = f"https://openweathermap.org/img/wn/{icon}@2x.png"
         self.ids.city_name_text.text = f"{city_name}"
+        self.ids.date_time.text = f"{date_time}"
 
 class MainScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def weather_search(self):
-        try:
-            self.ids.weather_carousel.clear_widgets()
-            city = self.ids.city_field.text.strip().lower() #отримуємо з стайл кв назву міста
-            api_params = {
-                'q': city,
-                'appid': API_KEY
-            }
-            data = requests.get(API_URL, api_params)# запит до сервісу погоди з параметрами
-            response = data.json()#отримуємо в-дь у форматі json
-            description = response['weather'][0]["description"]
-            icon = response['weather'][0]["icon"]
-            humidity = response['main']["humidity"]
-            temp = response['main']["temp"]
-            feel_like = response['main']["feels_like"]
-            if 'rain' in response:
+    def get_weather_data(self, url, city):
+        api_params = {
+            'q': city,
+            'appid': API_KEY
+        }
+
+        data = requests.get(url, api_params)  # запит до сервісу погоди з параметрами
+        response = data.json()  # отримуємо в-дь у форматі json
+        return response
+
+    def add_weather_card(self, response):
+        # try:
+        description = response['weather'][0]["description"]
+        icon = response['weather'][0]["icon"]
+        humidity = response['main']["humidity"]
+        temp = response['main']["temp"]
+        feel_like = response['main']["feels_like"]
+        if 'rain' in response:
+            if '1h' in response['rain']:
                 rain = response['rain']["1h"]
             else:
-                rain = 0
-            wind = response["wind"]["speed"]
+                rain = response['rain']["3h"]
+        else:
+            rain = 0
+        wind = response["wind"]["speed"]
+        if "name" in response:
             city_name = response["name"]
-            print(city_name)
+        else:
+            city_name = self.city
 
-            new_card = WeatherCard(description, icon, humidity, temp, rain, wind, feel_like, city_name)
-            self.ids.weather_carousel.add_widget(new_card)
-            setting["last_search"] = city_name
-            save()
-        except:
-            print("такого міста немає")
-            print(city_name)
-            print(city)
+        if "dt_txt" in response:
+            date_time = response['dt_txt'][5:-3]
+        else:
+            date_time = "Зараз"
+
+
+        new_card = WeatherCard(description, icon, humidity, temp, rain, wind, feel_like, city_name, date_time)
+        self.ids.weather_carousel.add_widget(new_card)
+        # setting["last_search"] = city_name
+        # save()
+
+        # except:
+        #     print("такого міста немає")
+    def weather_search(self):
+        self.ids.weather_carousel.clear_widgets()
+        city = self.ids.city_field.text.strip().lower() #отримуємо з стайл кв назву міста
+        self.city = city
+
+        current_weather = self.get_weather_data(API_URL, city)
+        forecast = self.get_weather_data(FORECAST_URL, city)
+        if current_weather:
+            self.add_weather_card(current_weather)
+        if forecast:
+            for period in forecast['list']:
+                self.add_weather_card(period)
 
 class MainApp(MDApp):
     def build(self):
